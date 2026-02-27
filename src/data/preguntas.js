d5e99b559833a3e442c799f6aa3b8fdf31ae47e5
guillesselvamiota <guillesselvamiota@gmail.com>
2026-02-27 20:11:15 +0100

    Update preguntas.js

--- src/data/preguntas.js
+++ src/data/preguntas.js
@@ -1,9 +1,57 @@
 export const preguntasLey = [
-{
-  id: 1,
-  titulo: "TITULO I: Del homicidio y sus formas",
-  preguntas: "¿En qué precepto se tipifica el homicidio doloso básico?",
-  opciones: ["Art. 138.1", "Art. 139.1", "Art. 142.1"],
-  correcta: 1,
-}
+  {
+    "id": 1,
+    "titulo": "TITULO I: Del homicidio y sus formas",
+    "preguntas": "¿En qué precepto se tipifica el homicidio doloso básico?",
+    "opciones": [
+      "Art. 138.1",
+      "Art. 139.1",
+      "Art. 142.1"
+    ],
+    "correcta": 1
+  },
+  {
+    "id": 2,
+    "titulo": "TITULO I: Del homicidio y sus formas",
+    "preguntas": "¿En qué precepto se regula la imposición de la pena superior en grado cuando el homicidio concurre con circunstancias del art. 140.1 o con delito de atentado del art. 550?",
+    "opciones": [
+      "Art. 138.2",
+      "Art. 139.2",
+      "Art. 140.1"
+    ],
+    "correcta": 1
+  },
+  {
+    "id": 3,
+    "titulo": "TITULO I: Del homicidio y sus formas",
+    "preguntas": "¿En qué precepto se tipifica el asesinato cuando concurre alevosía?",
+    "opciones": [
+      "Art. 139.1.1ª",
+      "Art. 139.2",
+      "Art. 140.1.1ª"
+    ],
+    "correcta": 1
+  },
+  {
+    "id": 4,
+    "titulo": "TITULO I: Del homicidio y sus formas",
+    "preguntas": "¿En qué precepto se regula el asesinato cuando la víctima sea menor de dieciséis años?",
+    "opciones": [
+      "Art. 139.1",
+      "Art. 140.1.1ª",
+      "Art. 140.2"
+    ],
+    "correcta": 2
+  },
+  {
+    "id": 5,
+    "titulo": "TITULO I: Del homicidio y sus formas",
+    "preguntas": "¿En qué precepto se establece la prisión permanente revisable cuando el reo hubiera sido condenado por la muerte de más de dos personas?",
+    "opciones": [
+      "Art. 140.1.3ª",
+      "Art. 140.2",
+      "Art. 139.2"
+    ],
+    "correcta": 2
+  }
 ];
